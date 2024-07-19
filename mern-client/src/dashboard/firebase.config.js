f7c4f0375273733cd7dfdb32d54213638ab575f3
sonu <mohdshamsheeraalam0786@gmail.com>
2024-07-19 13:03:05 +0530

    Changes on firebase.config.js

--- mern-client/src/dashboard/firebase.config.js
+++ mern-client/src/dashboard/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAimlYOZhbMRagfZnqfe7-QLb9ZtMl1me0",
-  authDomain: "mern-book-255c0.firebaseapp.com",
-  projectId: "mern-book-255c0",
-  storageBucket: "mern-book-255c0.appspot.com",
-  messagingSenderId: "21156175028",
-  appId: "1:21156175028:web:1177cc8d5235d189b84be1"
+  apiKey: "Enter your api key",
+  authDomain: "Enter your authDomain",
+  projectId: "Enter your project id",
+  storageBucket: "Enter your storageBucket",
+  messagingSenderId: "Enter your messagingSenderId",
+  appId: "Enter your appId"
 };
 
 // Initialize Firebase
